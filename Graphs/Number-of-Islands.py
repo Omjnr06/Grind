446182ca25846a4c7560260981dbaa0636ff844b
Omjnr06 <oj.magbadelo@gmail.com>
2026-08-19 13:20:56 +0300

    feat: added number of islands to graphs section

---
+++ Graphs/Number-of-Islands.py
@@ -0,0 +1,34 @@
+# Given an `m x n` 2D binary grid `grid` 
+# which represents a map of `'1'`s (land) and `'0'`s (water),
+#  return *the number of islands*.
+# An **island** is surrounded by water and is formed by connecting adjacent lands 
+# horizontally or vertically. 
+# You may assume all four edges of the grid are all surrounded by water.
+
+def numberOfIslands(grid):
+
+    visited = set()
+    islands = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def dfs(r,c):
+        
+        if r < 0 or c < 0 or r >= rows or c >= cols:
+            return
+
+        if grid[r][c] == "0" or (r,c) in visited:
+            return
+        
+        visited.add((r,c))
+
+        dfs(r+1,c)
+        dfs(r-1,c)
+        dfs(r+1,c+1)
+        dfs(r,c-1)
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1" and (r,c) not in visited:
+                islands += 1
+                dfs (r,c)
